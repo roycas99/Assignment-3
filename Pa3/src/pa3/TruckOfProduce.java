0b53b54a9a28432a3f8dfa2e0ec2399e3327a55d
Roycas <xg2163rd@go.minnstate.edu>
2020-03-12 22:49:06 -0500

    Truck class

---
+++ Pa3/src/pa3/TruckOfProduce.java
@@ -0,0 +1,25 @@
+package pa3;
+
+public class TruckOfProduce {
+	
+public TruckOfProduce(int size) {
+	
+	
+	Produce[] produce=new Produce[size];
+	for(int i=0; i<produce.length;i++) {
+		
+		
+	}
+	
+	for(int x=0;x<produce.length;x++)
+		System.out.println(produce[x]);
+
+}
+//public void addProduce(String name) {
+//	if(name.equals())
+	
+	
+}
+	
+
+
